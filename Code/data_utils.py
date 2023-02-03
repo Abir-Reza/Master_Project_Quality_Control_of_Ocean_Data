19d103a9edb1dd1f0dba7d931b8b299027a0c99a
Mutasim Ansari <mutasim.ansari@macio.de>
2023-02-03 20:00:43 +0100

    created data files directory

--- Code/data_utils.py
+++ Code/data_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 def process_train_data(num_signals,seq_length,seq_step):
-    train = np.load('././data/kdd99_train.npy',allow_pickle=True)
+    train = np.load('././data/train.npy',allow_pickle=True)
     print('Loaded train data')
     m, n = train.shape  # m=562387, n=35
 
@@ -46,7 +46,7 @@ def process_train_data(num_signals,seq_length,seq_step):
     return samples, labels
 
 def process_test_data(num_signals,seq_length,seq_step):
-    test = np.load('././data/kdd99_test.npy',allow_pickle=True)
+    test = np.load('././data/test.npy',allow_pickle=True)
     print('load kdd99_test from .npy')
 
     m, n = test.shape  # m1=494021, n1=35
