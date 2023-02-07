ed6215c9efc0ae9504b7ba44cc47fece6fea7a6e
Mutasim Ansari <mutasim.ansari@macio.de>
2023-02-07 12:51:39 +0100

    changed setting file outlier detection and path in outlier.py

--- Code/outlier_detection.py
+++ Code/outlier_detection.py
@@ -32,10 +32,10 @@ g_loss = tf.keras.losses.BinaryCrossentropy(from_logits=False)
 d_loss = tf.keras.losses.BinaryCrossentropy(from_logits=False)
 
 # load saved generator and discriminators
-loaded_discriminator = tf.saved_model.load(settings['saved_model_path'] + 'discriminators\model_seq_' + str(seq_length)+ '_' + settings['exp'] + '/')
+loaded_discriminator = tf.saved_model.load(settings['saved_model_path'] + 'discriminators/model_seq_' + str(seq_length)+ '_' + settings['exp'] + '/')
 discriminator = loaded_discriminator.signatures["serving_default"]
 
-loaded_generator = tf.saved_model.load(settings['saved_model_path'] + 'generators\model_seq_' + str(seq_length) + '_' + settings['exp'] + '/')
+loaded_generator = tf.saved_model.load(settings['saved_model_path'] + 'generators/model_seq_' + str(seq_length) + '_' + settings['exp'] + '/')
 generator = loaded_generator.signatures["serving_default"]
 
 def D_loss(real_data,fake_data):
