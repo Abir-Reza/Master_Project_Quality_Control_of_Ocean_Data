be7553b8c59a005f8065072bc35424340fa65ab2
Abir Reza <abir.cse.cou@gmail.com>
2023-02-14 21:39:33 +0100

    Update data_utilsFin.py
    .

--- Code/data_utilsFin.py
+++ Code/data_utilsFin.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 def process_train_data(num_signals,seq_length,seq_step, augment = None):
     if augment:
-        train = np.load('/home/abir/Study/Winter22_23/Project/TEST/Code/settings/data/contaminated_train.npy',allow_pickle=True)
+        train = np.load('/home/students/MAD-GAN/Master_Project_Quality_Control_of_Ocean_Data/Code/settings/data/contaminated_train.npy',allow_pickle=True)
     else:
-        train = np.load('/home/abir/Study/Winter22_23/Project/TEST/Code/settings/data/kdd99_train.npy',allow_pickle=True)
+        train = np.load('/home/students/MAD-GAN/Master_Project_Quality_Control_of_Ocean_Data/Code/settings/data/kdd99_train.npy',allow_pickle=True)
     print('Loaded train data')
     m, n = train.shape  # m=562387, n=35
 
@@ -44,7 +44,7 @@ def process_train_data(num_signals,seq_length,seq_step, augment = None):
     return samples, labels
 
 def process_test_data(num_signals,seq_length,seq_step):
-    test = np.load('/home/abir/Study/Winter22_23/Project/TEST/Code/settings/data/kdd99_test.npy',allow_pickle=True)
+    test = np.load('/home/students/MAD-GAN/Master_Project_Quality_Control_of_Ocean_Data/Code/settings/data/kdd99_test.npy',allow_pickle=True)
     print('load kdd99_test from .npy')
 
     m, n = test.shape  # m1=494021, n1=35
@@ -54,7 +54,7 @@ def process_test_data(num_signals,seq_length,seq_step):
     samples = test[:, 0:n - 1]
     labels = test[:, n - 1]
     labels = labels.astype(int)
-    np.save('/home/abir/Study/Winter22_23/Project/TEST/Code/settings/data/' + str(len(labels)), labels)
+    np.save('/home/students/MAD-GAN/Master_Project_Quality_Control_of_Ocean_Data/Code/settings/data/' + str(len(labels)), labels)
     idx = np.asarray(list(range(0, m)))  # record the idx of each point
     # -- apply PCA dimension reduction for multi-variate GAN-AD -- #
     X_a = samples
@@ -99,7 +99,7 @@ def get_batch(samples, batch_size, batch_idx, labels=None):
             return samples[start_pos:end_pos], labels[start_pos:end_pos]
 
 def seeder(num_signals,seq_length,seq_step,proximity):
-    test = np.load('/home/abir/Study/Winter22_23/Project/TEST/Code/settings/data/kdd99_test.npy',allow_pickle=True)
+    test = np.load('/home/students/MAD-GAN/Master_Project_Quality_Control_of_Ocean_Data/Code/settings/data/kdd99_test.npy',allow_pickle=True)
     m, n = test.shape 
     samples = test[:, 0:n - 1]
     seed = np.zeros([m,n-1])
